403bb91778fab33fb96fecc805a909fbc92715ec
Alejandro Beltre <127040596+AlejandroBeltre@users.noreply.github.com>
2024-07-09 19:58:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 DESCRIPTION = 'Python package for kataCheckout'
 LONG_DESCRIPTION = 'This is a python package for the kata 09: back to the checkout, which is a kata from codewars.com'
 
